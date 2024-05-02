0cd19a34aa449323e466c8ac377d4421895da589
khaoula <bouzartikhaoula@gmail.com>
2024-05-02 23:40:41 +0200

    modufication

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
+// <!-- start seting part -->
+let setingDiv=document.querySelector(".toggele-settings");
+let divAddOpen=document.querySelector(".setting-box")
+setingDiv.onclick=function () {
+    divAddOpen.classList.add("opened")
+
+}
+
+// <!-- end seting part -->
+
+
 
 // <!-- start landing page -->
-let landingPage=document.querySelector(".food-imege")
+let landingPage=document.querySelector(".food-imege");
 
 let arrayImage=["01.jpg", "02.jpg","03.jpg"];
 
 setInterval(()=>{
     let randomNumber=Math.floor(Math.random()*arrayImage.length);
+    
     landingPage.style.backgroundImage='url("../image/food/'+ arrayImage[randomNumber] +'")';
-},4000);
+},3000);
+
 // <!-- end landing page -->
+
+
